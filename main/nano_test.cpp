3caa0e24824920337858c61967a6288f7e357840
justinfung <qf28@sussex.ac.uk>
2023-12-29 13:23:28 +0000

    add model

--- main/nano_test.cpp
+++ main/nano_test.cpp
@@ -14,12 +14,14 @@ int main(int argc, char** argv)
     // Nanodet 
     Nanodet nanodet;
     // nanodet.load_param("../config/nanodet-plus-m_416-int8.json");
-    // nanodet.load_param("../config/nanodet-plus-m-1.5x_416_int8.json");
+    nanodet.load_param("../config/nanodet-plus-m-1.5x_416_int8.json");
     // nanodet.load_param("../config/nanodet-plus-m-1.5x_416.json");
-    nanodet.load_param("../config/nanodet-plus-m_416.json");
+    // nanodet.load_param("../config/nanodet-plus-m_416.json");
+    // nanodet.load_param("../config/nanodet-plus-m-1.5x_416-opt.json");
 
 
-    cv::Mat bgr = cv::imread("../image/1 EYFejGUjvjPcc4PZTwoufw.jpg", 1);
+    // cv::Mat bgr = cv::imread("../image/1 EYFejGUjvjPcc4PZTwoufw.jpg", 1);
+    cv::Mat bgr = cv::imread("../image/3.jpg", 1);
     // cv::Mat bgr = cv::imread("../image/3.jpg", 1);
     ncnn::Mat input = ncnn::Mat::from_pixels(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, bgr.cols, bgr.rows);
 
