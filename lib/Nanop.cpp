ad859386132aa271e6647a518df783d1b51b6e00
Justin Fung <qf28@sussex.ac.uk>
2023-12-30 14:12:35 +0000

    add required function for multistage inference

--- lib/Nanop.cpp
+++ lib/Nanop.cpp
@@ -3,6 +3,200 @@
 
 #include "cpu.h"
 
+static inline float intersection_area(const Object& a, const Object& b)
+{
+    cv::Rect_<float> inter = a.rect & b.rect;
+    return inter.area();
+}
+
+static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold)
+{
+    picked.clear();
+
+    const int n = faceobjects.size();
+
+    std::vector<float> areas(n);
+    for (int i = 0; i < n; i++)
+    {
+        areas[i] = faceobjects[i].rect.width * faceobjects[i].rect.height;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        const Object& a = faceobjects[i];
+
+        int keep = 1;
+        for (int j = 0; j < (int)picked.size(); j++)
+        {
+            const Object& b = faceobjects[picked[j]];
+
+            // intersection over union
+            float inter_area = intersection_area(a, b);
+            float union_area = areas[i] + areas[picked[j]] - inter_area;
+            // float IoU = inter_area / union_area
+            if (inter_area / union_area > nms_threshold)
+                keep = 0;
+        }
+
+        if (keep)
+            picked.push_back(i);
+    }
+}
+
+
+static void qsort_descent_inplace(std::vector<Object>& faceobjects, int left, int right)
+{
+    int i = left;
+    int j = right;
+    float p = faceobjects[(left + right) / 2].prob;
+
+    while (i <= j)
+    {
+        while (faceobjects[i].prob > p)
+            i++;
+
+        while (faceobjects[j].prob < p)
+            j--;
+
+        if (i <= j)
+        {
+            // swap
+            std::swap(faceobjects[i], faceobjects[j]);
+
+            i++;
+            j--;
+        }
+    }
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            if (left < j) qsort_descent_inplace(faceobjects, left, j);
+        }
+        #pragma omp section
+        {
+            if (i < right) qsort_descent_inplace(faceobjects, i, right);
+        }
+    }
+}
+
+static void qsort_descent_inplace(std::vector<Object>& faceobjects)
+{
+    if (faceobjects.empty())
+        return;
+
+    qsort_descent_inplace(faceobjects, 0, faceobjects.size() - 1);
+}
+
+
+static void generate_proposals(ncnn::Mat& cls_pred, ncnn::Mat& dis_pred, int stride, const ncnn::Mat& in_pad, float prob_threshold, std::vector<Object>& objects)
+{
+    // cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
+    dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h); // 2 hwc
+
+    const int num_grid_x = cls_pred.w;
+    const int num_grid_y = cls_pred.h;
+    const int num_class = cls_pred.c;
+    const int cstep_cls = cls_pred.cstep;
+
+    const int reg_max_1 = dis_pred.w / 4;
+    const int hstep_dis = dis_pred.cstep;
+
+    // std::cout << "num_grid_x " << num_grid_x << std::endl;
+    // std::cout << "num_grid_y " << num_grid_y << std::endl;
+    // std::cout << "num_class " << num_class << std::endl;
+    // std::cout << "cstep_cls " << cstep_cls << std::endl;
+    // std::cout << "reg_max_1 " << reg_max_1 << std::endl;
+    // std::cout << "hstep_dis " << hstep_dis << std::endl;    
+
+    for (int i = 0; i < num_grid_y; i++)
+    {
+        for (int j = 0; j < num_grid_x; j++)
+        {
+            float *score_ptr = cls_pred.row(i) + j;
+            float max_score = -FLT_MAX;
+            int max_label = -1;
+
+            for (int cls = 0; cls < num_class; cls++)
+            {
+                if (score_ptr[cls * cstep_cls] > max_score)
+                {
+                    max_score = score_ptr[cls * cstep_cls];
+                    max_label = cls;
+                }
+            }
+
+            if (max_score >= prob_threshold)
+            {
+                ncnn::Mat bbox_pred(reg_max_1, 4, (void*) dis_pred.row(j) + hstep_dis);
+                // std::cout << "max_score " << max_score << std::endl;
+                // std::cout << "max_label " << max_label << std::endl;
+                {
+                    ncnn::Layer* softmax = ncnn::create_layer("Softmax");
+
+                    ncnn::ParamDict pd;
+                    pd.set(0, 1); // axis
+                    pd.set(1, 1);
+                    softmax->load_param(pd);
+
+                    ncnn::Option opt;
+                    opt.num_threads = 1;
+                    opt.use_packing_layout = false;
+
+                    softmax->create_pipeline(opt);
+                    softmax->forward_inplace(bbox_pred, opt);
+                    softmax->destroy_pipeline(opt);
+
+                    delete softmax;
+                }
+
+                float pred_ltrb[4];
+                for (int k = 0; k < 4; k++)
+                {
+                    float dis = 0.f;
+                    const float* dis_after_sm = bbox_pred.row(k);
+                    for (int l = 0; l < reg_max_1; l++)
+                    {
+                        dis += l * dis_after_sm[l];
+                    }
+                    pred_ltrb[k] = dis * stride;
+                }
+
+                float x_center = j * stride;
+                float y_center = i * stride;
+
+                float xmin = x_center - pred_ltrb[0];
+                float ymin = y_center - pred_ltrb[1];
+                float xmax = x_center + pred_ltrb[2];
+                float ymax = y_center + pred_ltrb[3];
+
+                Object obj;
+                // obj.rect.x = x_center - pred_ltrb[0];
+                // obj.rect.y = y_center - pred_ltrb[1];
+                // obj.rect.width =  pred_ltrb[2] + pred_ltrb[0];
+                // obj.rect.height = pred_ltrb[3] + pred_ltrb[1];
+                obj.rect.x = xmin;
+                obj.rect.y = ymin;
+                obj.rect.width = xmax - xmin;
+                obj.rect.height = ymax - ymin;
+                obj.label = max_label;
+                obj.prob = max_score;
+                objects.push_back(obj);
+
+                // std::cout << "obj.rect.x " << obj.rect.x << std::endl;
+                // std::cout << "obj.rect.y " << obj.rect.y << std::endl;
+                // std::cout << "obj.rect.width " << obj.rect.width << std::endl;
+                // std::cout << "obj.rect.height " << obj.rect.height << std::endl;
+                // std::cout << "obj.label " << obj.label << std::endl;
+                // std::cout << "obj.prob " << obj.prob << std::endl;
+            }
+
+        }
+    }
+}
+
+
 NanodetP::NanodetP()
 {
     blob_pool_allocator.set_size_compare_ratio(0.f);
@@ -82,22 +276,25 @@ int NanodetP::detect(const cv::Mat& rgb, std::vector<Object>& objects, float pro
     ex.input("data", in_pad);
 
     std::vector<Object> proposals; // All objects
+
     // stride 8
     {
         ncnn::Mat cls_pred;
         ncnn::Mat dis_pred;
         ex.extract("cls8", cls_pred);
         ex.extract("dis8", dis_pred);
-        cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
-        dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
+        // cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
+        // dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
 
-        std::cout << "cls8: c " << cls_pred.c << std::endl;
-        std::cout << "cls8: h " << cls_pred.h << std::endl;
-        std::cout << "cls8: w " << cls_pred.w << std::endl;
-        std::cout << "dis8: c " << dis_pred.c << std::endl;
-        std::cout << "dis8: h " << dis_pred.h << std::endl;
-        std::cout << "dis8: w " << dis_pred.w << std::endl;
-        std::cout << std::endl;
+        // std::cout << "cls8: c " << cls_pred.c << std::endl;
+        // std::cout << "cls8: h " << cls_pred.h << std::endl;
+        // std::cout << "cls8: w " << cls_pred.w << std::endl;
+        // std::cout << "dis8: c " << dis_pred.c << std::endl;
+        // std::cout << "dis8: h " << dis_pred.h << std::endl;
+        // std::cout << "dis8: w " << dis_pred.w << std::endl;
+        std::vector<Object> obj8;
+        generate_proposals(cls_pred, dis_pred, 8, in_pad, prob_threshold, obj8);
+        proposals.insert(proposals.end(), obj8.begin(), obj8.end());
     }
 
     // stride 16
@@ -106,16 +303,18 @@ int NanodetP::detect(const cv::Mat& rgb, std::vector<Object>& objects, float pro
         ncnn::Mat dis_pred;
         ex.extract("cls16", cls_pred);
         ex.extract("dis16", dis_pred);
-        cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
-        dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
+        // cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
+        // dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
 
-        std::cout << "cls16: c " << cls_pred.c << std::endl;
-        std::cout << "cls16: h " << cls_pred.h << std::endl;
-        std::cout << "cls16: w " << cls_pred.w << std::endl;
-        std::cout << "dis16: c " << dis_pred.c << std::endl;
-        std::cout << "dis16: h " << dis_pred.h << std::endl;
-        std::cout << "dis16: w " << dis_pred.w << std::endl;
-        std::cout << std::endl;
+        // std::cout << "cls16: c " << cls_pred.c << std::endl;
+        // std::cout << "cls16: h " << cls_pred.h << std::endl;
+        // std::cout << "cls16: w " << cls_pred.w << std::endl;
+        // std::cout << "dis16: c " << dis_pred.c << std::endl;
+        // std::cout << "dis16: h " << dis_pred.h << std::endl;
+        // std::cout << "dis16: w " << dis_pred.w << std::endl;
+        std::vector<Object> obj16;
+        generate_proposals(cls_pred, dis_pred, 16, in_pad, prob_threshold, obj16);
+        proposals.insert(proposals.end(), obj16.begin(), obj16.end());
     }
 
     // stride 32
@@ -124,16 +323,19 @@ int NanodetP::detect(const cv::Mat& rgb, std::vector<Object>& objects, float pro
         ncnn::Mat dis_pred;
         ex.extract("cls32", cls_pred);
         ex.extract("dis32", dis_pred);
-        cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
-        dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
+        // cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
+        // dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
 
-        std::cout << "cls32: c " << cls_pred.c << std::endl;
-        std::cout << "cls32: h " << cls_pred.h << std::endl;
-        std::cout << "cls32: w " << cls_pred.w << std::endl;
-        std::cout << "dis32: c " << dis_pred.c << std::endl;
-        std::cout << "dis32: h " << dis_pred.h << std::endl;
-        std::cout << "dis32: w " << dis_pred.w << std::endl;
-        std::cout << std::endl;
+        // std::cout << "cls32: c " << cls_pred.c << std::endl;
+        // std::cout << "cls32: h " << cls_pred.h << std::endl;
+        // std::cout << "cls32: w " << cls_pred.w << std::endl;
+        // std::cout << "dis32: c " << dis_pred.c << std::endl;
+        // std::cout << "dis32: h " << dis_pred.h << std::endl;
+        // std::cout << "dis32: w " << dis_pred.w << std::endl;
+        std::vector<Object> obj32;
+        generate_proposals(cls_pred, dis_pred, 16, in_pad, prob_threshold, obj32);
+        proposals.insert(proposals.end(), obj32.begin(), obj32.end());
+        
     }
 
     // stride 64
@@ -142,16 +344,65 @@ int NanodetP::detect(const cv::Mat& rgb, std::vector<Object>& objects, float pro
         ncnn::Mat dis_pred;
         ex.extract("cls64", cls_pred);
         ex.extract("dis64", dis_pred);
-        cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
-        dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
+        // cls_pred = cls_pred.reshape(cls_pred.c, cls_pred.w, cls_pred.h);
+        // dis_pred = dis_pred.reshape(dis_pred.c, dis_pred.w, dis_pred.h);
 
-        std::cout << "cls64: c " << cls_pred.c << std::endl;
-        std::cout << "cls64: h " << cls_pred.h << std::endl;
-        std::cout << "cls64: w " << cls_pred.w << std::endl;
-        std::cout << "dis64: c " << dis_pred.c << std::endl;
-        std::cout << "dis64: h " << dis_pred.h << std::endl;
-        std::cout << "dis64: w " << dis_pred.w << std::endl;
-        std::cout << std::endl;
+        // std::cout << "cls64: c " << cls_pred.c << std::endl;
+        // std::cout << "cls64: h " << cls_pred.h << std::endl;
+        // std::cout << "cls64: w " << cls_pred.w << std::endl;
+        // std::cout << "dis64: c " << dis_pred.c << std::endl;
+        // std::cout << "dis64: h " << dis_pred.h << std::endl;
+        // std::cout << "dis64: w " << dis_pred.w << std::endl;
+        std::vector<Object> obj64;
+        generate_proposals(cls_pred, dis_pred, 16, in_pad, prob_threshold, obj64);
+        proposals.insert(proposals.end(), obj64.begin(), obj64.end());
+    }
+
+    qsort_descent_inplace(proposals);
+
+    std::vector<int> picked;
+    nms_sorted_bboxes(proposals, picked, nms_threshold);
+
+    int count = picked.size();
+    objects.resize(count);
+    for (int i = 0; i < count; i++)
+    {
+        objects[i] = proposals[picked[i]];
+
+        // adjust offset to original unpadded
+        float x0 = (objects[i].rect.x - (wpad / 2)) / scale;
+        float y0 = (objects[i].rect.y - (hpad / 2)) / scale;
+        float x1 = (objects[i].rect.x + objects[i].rect.width - (wpad / 2)) / scale;
+        float y1 = (objects[i].rect.y + objects[i].rect.height - (hpad / 2)) / scale;
+
+        // clip
+        x0 = std::max(std::min(x0, (float)(width - 1)), 0.f);
+        y0 = std::max(std::min(y0, (float)(height - 1)), 0.f);
+        x1 = std::max(std::min(x1, (float)(width - 1)), 0.f);
+        y1 = std::max(std::min(y1, (float)(height - 1)), 0.f);
+
+        objects[i].rect.x = x0;
+        objects[i].rect.y = y0;
+        objects[i].rect.width = x1 - x0;
+        objects[i].rect.height = y1 - y0;
+    }
+    struct
+    {
+        bool operator()(const Object& a, const Object& b) const
+        {
+            return a.rect.area() > b.rect.area();
+        }
+    } objects_area_greater;
+    std::sort(objects.begin(), objects.end(), objects_area_greater);
+
+    for (Object obj : objects)
+    {
+        std::cout << "obj.rect.x " << obj.rect.x << std::endl;
+        std::cout << "obj.rect.y " << obj.rect.y << std::endl;
+        std::cout << "obj.rect.width " << obj.rect.width << std::endl;
+        std::cout << "obj.rect.height " << obj.rect.height << std::endl;
+        std::cout << "obj.label " << obj.label << std::endl;
+        std::cout << "obj.prob " << obj.prob << std::endl;
     }
 
     return 0;
